e821d3c17df767ee9675005749a31d2fb1687915
yuguo6211 <yuguocug@sina.com>
2016-06-14 07:40:43 -0700

    aa

--- dist/js/controller/home/detail.js
+++ dist/js/controller/home/detail.js
@@ -4,9 +4,6 @@ $(document).ready(function () {
   //实例化vue对象
   var vueMvvm = new Vue({
     el: '#container',
-    data: {
-      header: HeaderJosn,
-      weblist: WebJosn
-    }
+    data: {}
   });
 });
--- dist/js/controller/home/mobile.js
+++ dist/js/controller/home/mobile.js
@@ -2,10 +2,13 @@
 
 $(document).ready(function () {
   //导航前端高亮
-  $('.header-list').find('li').eq(0).addClass('active');
+  $('.header-list').find('li').eq(3).addClass('active');
   //实例化vue对象
   var vueMvvm = new Vue({
     el: '#container',
-    data: {}
+    data: {
+      weblist: Mobile,
+      itemdata: ItemData
+    }
   });
 });
